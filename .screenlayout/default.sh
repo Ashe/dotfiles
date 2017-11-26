45bd9102a7cfe507d546cdfde24822322f50c704
Ashley Smith <ashley@aas.sh>
2017-11-26 21:54:54 +0000

    Added initial files

---
+++ .screenlayout/default.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-3 --off --output DVI-I-0 --off --output HDMI-0 --mode 1920x1080 --pos 4480x512 --rotate normal --output DP-5 --mode 1920x1080 --pos 0x512 --rotate normal --output DP-4 --off --output DVI-I-1 --off --output DP-2 --primary --mode 2560x1440 --rate 165 --pos 1920x0 --rotate normal --output DP-1 --off --output DP-0 --off
