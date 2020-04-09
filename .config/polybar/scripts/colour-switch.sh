232c299bb6ebf699d4d6d5ad372b4045662a23b8
Ashley Smith <ashley@aas.sh>
2020-04-09 18:33:40 +0100

    Polybar is now located at the top of the screen

--- .config/polybar/scripts/colour-switch.sh
+++ .config/polybar/scripts/colour-switch.sh
@@ -8,14 +8,13 @@ switch () {
 
 # Show options
 MENU="$(rofi -sep "|" -dmenu -i -p 'Select' \
-  -location 5 -columns 1 -xoffset -240 -yoffset -52 \
+  -location 3 -columns 1 -xoffset -240 -yoffset 52 \
   -width 12 -hide-scrollbar -line-padding 4 \
   -padding 20 -lines 2 \
   <<< " \
      cyberpunk|\
      flowers")"
-            case "$MENU" in
-				## Light Colors
-        *cyberpunk) switch cyberpunk ;;
-        *flowers)   switch flowers ;;
-            esac
+case "$MENU" in
+  *cyberpunk) switch cyberpunk ;;
+  *flowers)   switch flowers ;;
+esac
