a4e5293326aa593b4579a41a8c3430d8b7c8583d
Ashley Smith <ashley@aas.sh>
2020-05-26 20:49:22 +0100

    Disabled FZF keybindings by default

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,21 +3,6 @@
 # Allow vi keybindings
 fish_vi_key_bindings
 
-# Remove specific package keybindings
-bind --erase \ct '__fzf_find_file'
-bind --erase \ct '__fzf_find_file'
-bind --erase \cr '__fzf_reverse_isearch'
-bind --erase \ec '__fzf_cd'
-bind --erase \eC '__fzf_cd --hidden'
-bind --erase \cg '__fzf_open'
-bind --erase \co '__fzf_open --editor'
-bind -M insert --erase \ct '__fzf_find_file'
-bind -M insert --erase \cr '__fzf_reverse_isearch'
-bind -M insert --erase \ec '__fzf_cd'
-bind -M insert --erase \eC '__fzf_cd --hidden'
-bind -M insert --erase \cg '__fzf_open'
-bind -M insert --erase \co '__fzf_open --editor'
-
 # Open fuzzy file finder with CTRL+P
 bind \cp '__fzf_find_file'
 bind -M insert \cp '__fzf_find_file'
