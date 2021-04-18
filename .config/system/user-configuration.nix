9b11130ee2ea78dcf5756b3677c5efa2aef6a4fc
Ashley Smith <git@aas.sh>
2021-04-18 12:29:43 +0100

    Configured fish with home-manager

--- .config/system/user-configuration.nix
+++ .config/system/user-configuration.nix
@@ -44,6 +44,86 @@ with lib;
   # Enable discovery of fonts from installed packages
   fonts.fontconfig.enable = lib.mkForce true;
 
+  # Configure FISH
+  programs.fish = {
+    enable = true;
+
+    # Install plugins
+    plugins = [
+      
+      # 'Pure' prompt theme
+      {
+        name = "pure";
+        src = pkgs.fetchFromGitHub {
+          owner = "pure-fish";
+          repo = "pure";
+          rev = "9d17b2780165572bc6c181cd0e7c5d6dd13be5c1";
+          sha256 = "16f8jq2697l0hfv848bn7vzxnbzql4qbi9brkniilfxb1dnllhzb";
+        };
+      }
+
+      # Notifications when commands are completed
+      {
+        name = "done";
+        src = pkgs.fetchFromGitHub {
+          owner = "done";
+          repo = "franciscolourenco";
+          rev = "8fd2bc5c95a93b65452281459a167f1bbfb58990";
+          sha256 = "16f8jq2697l0hfv848bn7vzxnbzql4qbi9brkniilfxb1dnllhzb";
+        };
+      }
+
+    ];
+
+    # Add functions
+    functions = {
+      sudo = {
+        body = ''
+          if test "$argv" = !!
+            eval command sudo $history[1]
+          else
+            command sudo $argv
+          end
+        '';
+        description = ''
+          Replacement for Bash 'sudo !!' command to run last command using sudo
+        '';
+      };
+    };
+
+    # When shell is opened
+    interactiveShellInit = ''
+
+      # Allow vi keybindings
+      fish_vi_key_bindings
+
+      # Start tmux every session
+      if  status is-interactive &&
+          command -sq tmux &> /dev/null &&
+          not string match -q 'screen*' -- $TERM &&
+          not string match -q 'tmux*' -- $TERM &&
+          test -z "$TMUX"
+
+        # Check if there's an unattached session
+        mkdir -p $HOME/.cache/tmux
+        tmux list-sessions > $HOME/.cache/tmux/sessions
+        set sessions (cat $HOME/.cache/tmux/sessions | grep -v attached | cut -d: -f1)
+
+        # Reattach to any unattached sessions available
+        if set -q tmux_unattached[1]
+          exec tmux attach-session -t "$tmux_unattached[1]"
+
+        # Otherwise start a new session
+        else
+          exec tmux new-session
+        end
+      end
+    '';
+  };
+
+  # Enable fuzzy-file-finder
+  programs.fzf.enable = true;
+
   # Configure git
   programs.git = {
     enable = true;
