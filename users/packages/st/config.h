59f78fb67766e87991aa460a3093cefede309fd3
Ashley Smith <git@aas.sh>
2022-01-24 17:48:09 +0000

    Updated flake.lock, fixed st config

--- users/packages/st/config.h
+++ users/packages/st/config.h
@@ -129,8 +129,8 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
-static unsigned int defaultcs = 257;
-static unsigned int defaultrcs = 256;
+unsigned int defaultcs = 257;
+unsigned int defaultrcs = 256;
 
 /*
  * Default shape of cursor
