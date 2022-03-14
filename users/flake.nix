e531612950a34f1494a403fe0b1d293fe1cc6008
Ashley Smith <git@aas.sh>
2022-03-14 09:31:04 +0000

    Updated flake.nix and flake.lock, we now track nixpkgs separately and tell home-manager to follow it

--- users/flake.nix
+++ users/flake.nix
@@ -1,16 +1,28 @@
 {
   description = "User configuration flake";
   inputs = {
-    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nur, ... }@inputs: let overlays = [ nur.overlay ]; in {
+  outputs = { self, nur, ... }@inputs: {
     homeConfigurations = {
-      nixos = inputs.home-manager.lib.homeManagerConfiguration {
+      nixos = inputs.home-manager.lib.homeManagerConfiguration rec {
         username = "ashe";
         homeDirectory = "/home/ashe";
         system = "x86_64-linux";
-        configuration.imports = [ ./home.nix ];
+        configuration = {
+          imports = [ ./home.nix ];
+          nixpkgs = {
+            config.allowUnfree = true;
+            overlays = [
+              nur.overlay 
+            ];
+          };
+        };
       };
     };
   };
