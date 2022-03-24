c06cdaaebec0666c0cfc74c6d94dda61616f27d0
Ashley Smith <git@aas.sh>
2022-03-24 07:54:39 +0000

    Updated flake.nix and flake.lock, switched from amdvlk to radv drivers

--- system/configuration.nix
+++ system/configuration.nix
@@ -69,12 +69,8 @@
       driSupport = true;
       driSupport32Bit = true;
       extraPackages = with pkgs; [
-        amdvlk
         rocm-opencl-icd
         rocm-opencl-runtime
-      ];
-      extraPackages32 = with pkgs; [
-        driversi686Linux.amdvlk
       ];
     };
     steam-hardware.enable = true;
