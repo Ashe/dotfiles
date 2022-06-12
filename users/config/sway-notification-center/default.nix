7794a4cdb6f00360295db086ec5b80aabc14ebed
Ashley Smith <git@aas.sh>
2022-06-12 15:34:51 +0100

    Sway notification center now has theme colours

--- users/config/sway-notification-center/default.nix
+++ users/config/sway-notification-center/default.nix
@@ -1,8 +1,18 @@
 { theme, ... }:
 
 {
-  # Configure sway notification center
-  xdg.configFile.swaync = {
-    source = ./config;
+  # Manage sway notification center settings
+  xdg.configFile.swaync-settings = {
+    source = ./config.json;
+    target = "swaync/config.json";
+  };
+
+  # Manage sway notification center styling
+  xdg.configFile.swaync-style = {
+    target = "swaync/style.css";
+    text = builtins.replaceStrings 
+      (builtins.attrNames theme.colourscheme) 
+      (builtins.attrValues theme.colourscheme) 
+      (builtins.readFile ./style.css);
   };
 }
