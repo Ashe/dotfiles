42f5f23cb64a2a4210786dbd3f155e4cee77474e
Ashley Smith <git@aas.sh>
2022-11-08 11:09:34 +0000

    Added home configuration ashe@artemis

---
+++ home/configurations/ashe@artemis.nix
@@ -0,0 +1,57 @@
+_: { config, lib, pkgs, ... }:
+
+{
+  ##################
+  # Custom modules #
+  ##################
+
+  neovim.enable = true;
+  starship.enable = true;
+  zsh.enable = true;
+
+  ##################
+  # Configurations #
+  ##################
+
+  # Configure user experience
+  home = {
+
+    # Home variables
+    stateVersion = "22.11";
+
+    # Set environment variables
+    sessionVariables = {
+      FILE = "ranger";
+    };
+
+    # Packages to install
+    packages = with pkgs; [
+
+      # Programs
+      ranger
+    ];
+  };
+
+  # Configure programs
+  programs = {
+
+    # Configure git
+    git = {
+      enable = true;
+      userName  = "ashe";
+      userEmail = "git@aas.sh";
+    };
+
+    # Enable lazygit client
+    lazygit.enable = true;
+
+    # Enable nix-index file database for nixpkgs
+    nix-index.enable = true;
+
+    # Fuzzy file finding
+    fzf.enable = true;
+
+    # Cheat sheets
+    navi.enable = true;
+  };
+}
--- home/configurations/default.nix
+++ home/configurations/default.nix
@@ -1,3 +1,4 @@
 { self, ... } @ inputs: {
   "ashe@athena" = self.lib.mkHome "ashe" "athena" "x86_64-linux";
+  "ashe@artemis" = self.lib.mkHome "ashe" "artemis" "x86_64-linux";
 }
