1abfdbb099c1422e6ab673790db91ec675e5a206
ashe <git@aas.sh>
2022-12-31 17:58:23 +0000

    Neovim: updated treesitter config to add increemental selection

--- home/modules/neovim/plugins/nvim-treesitter/config.lua
+++ home/modules/neovim/plugins/nvim-treesitter/config.lua
@@ -2,7 +2,7 @@
 -- nvim-treesitter
 ----------------------------------
 
-require'nvim-treesitter.configs'.setup({
+require('nvim-treesitter.configs').setup({
 
   -- A list of parser names, or "all"
   ensure_installed = all,
@@ -14,9 +14,70 @@ require'nvim-treesitter.configs'.setup({
   parser_install_dir = "~/.local/share/nvim-treesitter/parsers",
 
   -- Enable syntax highlighting
-  highlight = {
+  highlight = { enable = true },
+
+  -- Intelligently select code incrementally
+  incremental_selection = {
     enable = true,
-  }
+    keymaps = {
+      init_selection = '<c-space>',
+      node_incremental = '<c-space>',
+      scope_incremental = '<c-s>',
+      node_decremental = '<c-backspace>',
+    },
+  },
+
+  -- Define custom text objects for navigation and text manipulation
+  textobjects = {
+
+    -- Define custom selections
+    select = {
+      enable = true,
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ['aa'] = '@parameter.outer',
+        ['ia'] = '@parameter.inner',
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      },
+    },
+
+    -- Define custom movements
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
+
+    -- Allow swapping
+    swap = {
+      enable = true,
+      swap_next = {
+        ['<leader>a'] = '@parameter.inner',
+      },
+      swap_previous = {
+        ['<leader>A'] = '@parameter.inner',
+      },
+    },
+  },
 })
 
 -- Ensure parser location added to path
--- home/modules/neovim/plugins/telescope/config.lua
+++ home/modules/neovim/plugins/telescope/config.lua
@@ -7,11 +7,11 @@ require('telescope').setup()
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
+map('n', '<leader><leader>', '<Cmd>Telescope buffers<CR>', opts)
 map('n', '<C-p>', '<Cmd>Telescope find_files<CR>', opts)
 map('n', '<leader>ff', '<Cmd>Telescope find_files<CR>', opts)
 map('n', '<leader>fr', '<Cmd>Telescope oldfiles<CR>', opts)
 map('n', '<leader>fg', '<Cmd>Telescope live_grep<CR>', opts)
-map('n', '<leader>fb', '<Cmd>Telescope buffers<CR>', opts)
 map('n', '<leader>fh', '<Cmd>Telescope help_tags<CR>', opts)
 map('n', '<leader>?', '<Cmd>Telescope commands<CR>', opts)
 map('n', '<C-S-p>', '<Cmd>Telescope commands<CR>', opts)
