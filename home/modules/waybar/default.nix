d844c2629c909bc0bf766a4bdf8017379599a370
ashe <git@aas.sh>
2023-04-20 12:38:49 +0100

    Improved Waybar config

--- home/modules/waybar/default.nix
+++ home/modules/waybar/default.nix
@@ -13,234 +13,171 @@ _: { config, lib, pkgs, theme, ... }:
       target = "waybar/extra";
     };
 
-    # Enable Waybar
+    # Install extra programs for use with waybar
+    home.packages = with pkgs; [
+
+      # For controlling spotify etc
+      playerctl
+
+      # For controlling volume via pactl
+      pulseaudio-ctl
+    ];
+
+    # Configure waybar
     programs.waybar = {
+
+      # Install waybar
       enable = true;
 
+      # Enable experimental features
+      package = pkgs.waybar.overrideAttrs (oa: { mesonFlags = oa.mesonFlags ++ [ "-Dexperimental=true" ]; });
+
       # Configure Waybar
-      settings =
-      [
-        {
-          # Basics
-          layer = "top";
-          position = "right";
+      settings = [{
 
-          # Left modules
-          modules-left = [
-            "custom/menu"
-            "custom/terminal"
-            "custom/filebrowser"
-            "custom/browser"
-            "custom/spotify-shortcut"
-            "custom/discord"
-            "sway/mode"
-          ];
+        # Basics
+        layer = "top";
+        position = "right";
+        height = 200;
+        margin-right = 10;
 
-          # Right modules
-          modules-right = [
-            "custom/spotify-monitor"
-            "custom/spotify-play-pause"
-            "cpu"
-            "network"
-            "pulseaudio"
-            "tray"
-            "clock#day"
-            "clock#date"
-            "clock"
-            "custom/notifications"
-          ];
+        # Center modules
+        modules-center = [
+          "custom/home"
+          "wlr/workspaces"
+          "custom/spotify"
+          "tray"
+          "network"
+          "pulseaudio#icon"
+          "pulseaudio#percentage"
+          "battery#icon"
+          "battery#percentage"
+          "clock#calendar-icon"
+          "clock#day"
+          "clock#date"
+          "clock#time-icon"
+          "clock#time"
+        ];
 
-          # Current time and date
-          "clock#day" = {
-            format = "{:%A}";
-            tooltip-format = "<big>  {:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+        # Home module
+        "custom/home" = {
+          format = "";
+          on-click = "$MENU";
+          on-click-right = "wlogout";
+          on-scroll-up = "hyprctl dispatch workspace e-1";
+          on-scroll-down = "hyprctl dispatch workspace e+1";
+          tooltip = false;
+        };
+
+        # Workspace module
+        "wlr/workspaces" = {
+          format = "{icon}";
+          sort-by-number = true;
+          on-click = "activate";
+          on-scroll-up = "hyprctl dispatch workspace e-1";
+          on-scroll-down = "hyprctl dispatch workspace e+1";
+          on-click-backward = "hyprctl dispatch workspace e-1";
+          on-click-forward = "hyprctl dispatch workspace e+1";
+        };
+
+        # Spotify module
+        "custom/spotify" = {
+          format = "";
+          interval = 1;
+          exec-if = "pgrep spotify";
+          return-type = "json";
+          on-click = "playerctl -p spotify play-pause";
+          on-scroll-up = "playerctl -p spotify previous";
+          on-scroll-down = "playerctl -p spotify next";
+          tooltip = true;
+          escape = true;
+          exec = "/home/ashe/.config/waybar/extra/spotify.sh"; #@TODO: ADD THIS SCRIPT
+        };
+
+        # Tray module
+        tray = {
+          icon-size = 18;
+          spacing = 10;
+        };
+
+        # Network module
+        network = {
+          format-wifi = "";
+          format-ethernet = "󰈀";
+          format-linked = "{ifname} (No IP) 󰈁";
+          format-disconnected = "󰈂";
+          tooltip-format-ethernet = "󰈀 {ifname}";
+          tooltip-format-wifi = " {essid} ({signalStrength})";
+          tooltip-format-disconnected = "Disconnected";
+        };
+
+        # Pulseaudio module
+        "pulseaudio#icon" = {
+          on-click = "pactl set-sink-mute @DEFAULT_SINK@ toggle";
+          format = "{icon}";
+          format-muted = "";
+          format-icons = {
+            default = ["" ""];
           };
+        };
 
-          # Current time and date
-          "clock#date" = {
-            format = "{:%d %b}";
-            tooltip-format = "<big>  {:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-          };
+        # Pulseaudio text module
+        "pulseaudio#percentage" = {
+          on-click = "pactl set-sink-mute @DEFAULT_SINK@ toggle";
+          format = "{volume}";
+        };
 
-          # Current time and date
-          "clock" = {
-            format = "{:%I:%M %p}";
-            tooltip-format = "<big>  {:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+        # Battery module
+        "battery#icon" = {
+          states = {
+            warning = 30;
+            critical = 15;
           };
+          format = "{icon}";
+          format-warning = "<span foreground='#98bb6c'>{icon}</span>";
+          format-critical = "<span foreground='#e46876'>{icon}</span>";
+          format-charging = "<span foreground='#98bb6c'></span>";
+          format-plugged = "<span foreground='#98bb6c'></span>";
+          format-full = "<span foreground='#98bb6c'></span>";
+          format-icons = ["󱃍" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹"];
+        };
 
-          # CPU usage
-          cpu = {
-            format = "  {usage}%";
-          };
+        # Battery text module
+        "battery#percentage" = {
+          format = "{capacity}";
+          tooltip-format = "{time}";
+        };
 
-          # Network usage
-          network = {
-            format-ethernet = " ";
-            tooltip-format-ethernet = "  {bandwidthDownBits} - {ipaddr}";
-            format-wifi = " {essid}";
-            tooltip-format-wifi = "  {bandwidthDownBits} - {ifname} - {ipaddr}";
-            format-disconnected = "";
-            tooltip-format-disconnected = "No internet connection";
-          };
+        # Calendar icon module
+        "clock#calendar-icon" = {
+          format = "";
+          tooltip-format = " <big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+        };
 
-          # Volume slider
-          pulseaudio = {
-            format = "{icon}  {volume}";
-            format-bluetooth = "{icon} {volume}";
-            format-muted = "  {volume}";
-            format-icons = {
-              default = "";
-              headphone = "";
-              headset = "";
-            };
-            on-click = "pulseaudio-ctl mute";
-            on-click-middle = "pavucontrol";
-            on-click-right = "pavucontrol";
-            all-outputs = true;
-          };
+        # Clock text module (day of the week)
+        "clock#day" = {
+          format = "{:%a}";
+          tooltip-format = " <big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+        };
 
-          # Current sway mode (resize etc)
-          "sway/mode" = {
-            format = "  {}";
-          };
+        # Date text module (date)
+        "clock#date" = {
+          format = "{:%d}";
+          tooltip-format = " <big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+        };
 
-          # Current sway workspaces
-          "sway/workspaces" = {
-            format = "{icon}";
-            enable-bar-scroll = true;
-            disable-scroll-wraparound = true;
-            format-icons = {
-              default = "";
-              focused = "";
-              urgent = "";
-            };
-          };
+        # Time icon module
+        "clock#time-icon" = {
+          format = "󰥔";
+          tooltip-format = "󰥔 {:%I:%M %p}";
+        };
 
-          # App tray
-          tray = {
-            icon-size = 18;
-            spacing = 8;
-          };
-
-          # Menu shortcut
-          "custom/menu" = {
-            format = "";
-            on-click = "$MENU";
-            on-click-right = "wlogout";
-            tooltip = false;
-          };
-
-          # Terminal shortcut
-          "custom/terminal" = {
-            format = "";
-            on-click = "$TERMINAL";
-            tooltip = false;
-          };
-
-          # File browser shortcut
-          "custom/filebrowser" = {
-            format = "";
-            on-click = "$FILEGUI --new-window";
-            on-click-middle = "pkill $FILEGUI";
-            tooltip = false;
-          };
-
-          # Browser shortcut
-          "custom/browser" = {
-            format = "";
-            on-click = "$BROWSER";
-            on-click-middle = "pkill $BROWSER";
-            tooltip = false;
-          };
-
-          # Discord shortcut
-          "custom/discord" = {
-            format = "";
-            on-click = "discord";
-            on-click-middle = "pkill discord";
-            tooltip = false;
-          };
-
-          # Notification center
-          "custom/notifications" = {
-            tooltip = false;
-            format = "{icon}";
-            format-icons = {
-              notification = "<span foreground='red'><sup></sup></span>";
-              none = "";
-              dnd-notification = "<span foreground='red'><sup></sup></span>";
-              dnd-none = "";
-            };
-            return-type = "json";
-            exec-if = "which swaync-client";
-            exec = "swaync-client -swb";
-            on-click = "swaync-client -t -sw";
-            on-click-right = "swaync-client -d -sw";
-            escape = true;
-          };
-
-          # Shortcut for spotify
-          "custom/spotify-shortcut" = {
-            format = "";
-            on-click = "spotify";
-            on-click-middle = "pkill spotify";
-            tooltip = false;
-          };
-
-          # Continuously polls Spotify
-          "custom/spotify-monitor" = {
-            interval = 1;
-            exec = "/home/ashe/.config/waybar/extra/spotify/monitor.sh";
-            signal = 4;
-            tooltip = false;
-          };
-
-          # Shows detail about current Spotify song
-          "custom/spotify-metadata" = {
-            format = "  {}";
-            max-length = 500;
-            interval = "once";
-            return-type = "json";
-            exec = "/home/ashe/.config/waybar/extra/spotify/metadata.sh";
-            on-click = "/home/ashe/.config/waybar/extra/spotify/controls.sh";
-            signal = 5;
-          };
-
-          # Allows the playing or pausing of Spotify
-          "custom/spotify-play-pause" = {
-            format = "{}";
-            interval = "once";
-            return-type = "json";
-            exec = "/home/ashe/.config/waybar/extra/spotify/play-pause.sh";
-            on-click = "/home/ashe/.config/waybar/extra/spotify/controls.sh";
-            on-click-right = "/home/ashe/.config/waybar/extra/spotify/controls.sh next";
-            on-scroll-up = "/home/ashe/.config/waybar/extra/spotify/controls.sh next";
-            on-click-middle = "/home/ashe/.config/waybar/extra/spotify/controls.sh prev";
-            on-scroll-down = "/home/ashe/.config/waybar/extra/spotify/controls.sh prev";
-            signal = 5;
-          };
-
-          # Skip to next Spotify song
-          "custom/spotify-next" = {
-            format = "";
-            return-type = "json";
-            interval = "once";
-            exec = "/home/ashe/.config/waybar/extra/spotify/metadata.sh";
-            on-click = "/home/ashe/.config/waybar/extra/spotify/controls.sh next";
-            signal = 5;
-          };
-
-          # Skip to previous Spotify song
-          "custom/spotify-prev" = {
-            format = "";
-            interval = "once";
-            return-type = "json";
-            exec = "/home/ashe/.config/waybar/extra/spotify/metadata.sh";
-            on-click = "/home/ashe/.config/waybar/extra/spotify/controls.sh prev";
-            signal = 5;
-          };
-        }
-      ];
+        # Clock text module (Time of day)
+        "clock#time" = {
+          format = "{:%H\n%M}";
+          tooltip-format = "󰥔 {:%I:%M %p}";
+        };
+      }];
 
       # Appearance
       style = builtins.replaceStrings
