e115cc4533d56325a7ffd2c7c94eb4f64b8389cf
ashe <git@aas.sh>
2026-06-27 20:59:09 +0100

    artemis: Update config

--- home/configurations/ashley.rose@artemis.nix
+++ home/configurations/ashley.rose@artemis.nix
@@ -22,15 +22,32 @@
     sessionVariables = {
       EDITOR = "nvim";
       VISUAL = "nvim";
+      JAVA_HOME = pkgs.jdk21;
     };
+
+    sessionPath = [
+      "$HOME/.cargo/bin"
+    ];
 
     packages = with pkgs; [
       awscli2
       bat
       gnupg
+      rust-analyzer
       tree
+      git-credential-manager
+
+      (pkgs.quarto.overrideAttrs (oldAttrs: {
+        postPatch = (oldAttrs.postPatch or "") + ''
+          substituteInPlace bin/quarto.js \
+            --replace-fail "syntax-highlighting" "highlight-style"
+        '';
+      }))
     ];
   };
+
+  # Configure git to use git-credential-manager
+  programs.git.settings.credential.helper = "manager";
 
   # Fuzzy file finding
   programs.fzf.enable = true;
