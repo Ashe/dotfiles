29e867d5a4b12a0fcb5b0b16ea89bba14dcb7e86
ashe <git@aas.sh>
2026-07-16 08:03:20 +0100

    server: Fix module requiring non-null

--- nixos/modules/server/default.nix
+++ nixos/modules/server/default.nix
@@ -4,11 +4,11 @@
   # Custom module containing commonly required data for service modules
   options.server = {
     ip = lib.mkOption {
-      type = lib.types.str;
+      type = lib.types.nullOr lib.types.str;
       description = "Local IP address of this server";
     };
     domain = lib.mkOption {
-      type = lib.types.str;
+      type = lib.types.nullOr lib.types.str;
       description = "Local domain name for this server";
     };
     publicDomain = lib.mkOption {
