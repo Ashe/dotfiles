f8372c293f75a61ab31adc25c6b3a8a4e7ecaf12
ashe <git@aas.sh>
2026-07-16 08:03:20 +0100

    lab: Removed redundant import

--- nixos/configurations/lab/configuration.nix
+++ nixos/configurations/lab/configuration.nix
@@ -40,11 +40,6 @@
   # Configurations #
   ##################
 
-  imports = [
-    # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-  ];
-
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
