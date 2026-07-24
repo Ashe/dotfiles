358b137d18e1889bb17ec4ab325dfe7b954d138b
ashe <git@aas.sh>
2026-07-24 18:12:02 +0100

    zsh: Enable partial history search and jujutsu completion

--- home/modules/zsh/default.nix
+++ home/modules/zsh/default.nix
@@ -13,13 +13,27 @@
     # Configure zsh shell
     programs.zsh = {
 
+      ###########
+      # General #
+      ###########
+
       # Enable zsh
       enable = true;
 
       # Enable zsh features
       enableCompletion = true;
-      syntaxHighlighting.enable = true;
       enableVteIntegration = true;
+
+      # History management
+      history = {
+        size = 10000;
+        path = "$HOME/.zsh_history";
+        ignorePatterns = [
+          "rm *"
+          "pkill *"
+          "cp *"
+        ];
+      };
 
       # Configure shell aliases for zsh
       shellAliases = {
@@ -28,22 +42,63 @@
         run-with-xwayland = "env -u WAYLAND_DISPLAY";
       };
 
-      # Install plugins
+      # Additional init configuration
+      initContent = ''
+        ############################
+        # Additional configuration #
+        ############################
+
+        # Accept auto-suggestion
+        zvm_after_init_commands+=('bindkey "^L" autosuggest-accept')
+
+        # Substring history search
+        zvm_after_init_commands+=('bindkey "^P" history-substring-search-up')
+        zvm_after_init_commands+=('bindkey "^N" history-substring-search-down')
+
+        ${lib.optionalString (lib.hasAttr "jujutsu" pkgs) ''
+          # Jujutsu completions
+          if command -v jj >/dev/null 2>&1; then source <(jj util completion zsh); fi
+        ''}
+      '';
+
+      # Additional configuration for .zprofile
+      profileExtra = ''
+        ############################
+        # Additional configuration #
+        ############################
+
+        # Activate homebrew if installed
+        if [[ -x /opt/homebrew/bin/brew ]]; then
+          eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+        fi
+      '';
+
+      ###########
+      # Plugins #
+      ###########
+
+      syntaxHighlighting.enable = true;
+      historySubstringSearch.enable = true;
+
+      # Autosuggestions
+      autosuggestion = {
+        enable = true;
+        strategy = [
+          "history"
+          "completion"
+        ];
+      };
+
+      # Install extra plugins
       plugins = [
 
         # Vi keybindings
         {
           name = "zsh-vi-mode";
-          file = "./share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
+          file = "share/zsh-vi-mode/zsh-vi-mode.plugin.zsh";
           src = pkgs.zsh-vi-mode;
         }
 
-        # Autosuggestions
-        {
-          name = "zsh-autosuggestions";
-          file = "./share/zsh-autosuggestions/zsh-autosuggestions.zsh";
-          src = pkgs.zsh-autosuggestions;
-        }
       ];
     };
   };
