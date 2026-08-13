5183f93ae1aa17ed47fb16e96427179301d8ba37
ashe <git@aas.sh>
2026-08-13 18:56:52 +0100

    neovim: Update true-zen to not depend on which-key

--- home/modules/neovim/plugins/true-zen.lua
+++ home/modules/neovim/plugins/true-zen.lua
@@ -2,7 +2,8 @@ require("true-zen").setup({
 	-- Conguration goes here, or leave blank
 })
 
-require("which-key").add({ { "<leader>z", group = "Zen mode.." } })
+which_key_add({ { "<leader>z", group = "Zen mode..", mode = { "n", "v" } } })
+
 vim.keymap.set({ "n", "v" }, "<leader>zn", ":'<,'>TZNarrow<CR>", { desc = "Narrow" })
 vim.keymap.set("n", "<leader>zf", ":TZFocus<CR>", { desc = "Focus" })
 vim.keymap.set("n", "<leader>zm", ":TZMinimalist<CR>", { desc = "Minimalist" })
