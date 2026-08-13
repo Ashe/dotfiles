6abbf76c03d2ae63e55d4a3d3fc05d2c65caa88c
ashe <git@aas.sh>
2026-08-13 18:00:11 +0100

    wezterm: Format with stylua

--- home/modules/wezterm/config.lua
+++ home/modules/wezterm/config.lua
@@ -1,33 +1,33 @@
 -- Configure wezterm
 return {
 
-  -- Customise appearance
-  font = wezterm.font "FiraCode Nerd Font",
-  font_size = 14.0,
-  color_scheme = "Tokyo Night",
-  front_end = "WebGpu",
+	-- Customise appearance
+	font = wezterm.font("FiraCode Nerd Font"),
+	font_size = 14.0,
+	color_scheme = "Tokyo Night",
+	front_end = "WebGpu",
 
-  -- Disable fancy tab bar
-  use_fancy_tab_bar = false,
+	-- Disable fancy tab bar
+	use_fancy_tab_bar = false,
 
-  -- Disable sounds
-  audible_bell = "Disabled",
+	-- Disable sounds
+	audible_bell = "Disabled",
 
-  -- Customise keybindings
-  keys = {
+	-- Customise keybindings
+	keys = {
 
-    -- Command palette
-    { key = "k", mods = "CTRL|SHIFT", action = wezterm.action.ActivateCommandPalette },
+		-- Command palette
+		{ key = "k", mods = "CTRL|SHIFT", action = wezterm.action.ActivateCommandPalette },
 
-    -- Tabs
-    { key = "Enter", mods = "CTRL|SHIFT", action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
-    { key = "Backspace", mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentTab { confirm = true } },
-    { key = "j", mods = "CTRL|SHIFT", action = wezterm.action.ShowTabNavigator },
-    { key = "p", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(-1) },
-    { key = "n", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(1) },
+		-- Tabs
+		{ key = "Enter", mods = "CTRL|SHIFT", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+		{ key = "Backspace", mods = "CTRL|SHIFT", action = wezterm.action.CloseCurrentTab({ confirm = true }) },
+		{ key = "j", mods = "CTRL|SHIFT", action = wezterm.action.ShowTabNavigator },
+		{ key = "p", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(-1) },
+		{ key = "n", mods = "CTRL|SHIFT", action = wezterm.action.ActivateTabRelative(1) },
 
-    -- Copy / paste
-    { key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo "Clipboard" },
-    { key = "v", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom "Clipboard" },
-  },
+		-- Copy / paste
+		{ key = "c", mods = "CTRL|SHIFT", action = wezterm.action.CopyTo("Clipboard") },
+		{ key = "v", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
+	},
 }
