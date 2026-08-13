05dae1d974c1fe3a30eb707619c00980e7313a67
ashe <git@aas.sh>
2026-08-13 18:41:57 +0100

    neovim: Rebind filetree

--- home/modules/neovim/plugins/tree.lua
+++ home/modules/neovim/plugins/tree.lua
@@ -4,5 +4,4 @@ require("nvim-tree").setup()
 -- Keybindings
 
 -- Move to previous/next
-vim.keymap.set("n", "<leader>fo", "<Cmd>NvimTreeToggle<CR>", { desc = "Open file tree" })
 vim.keymap.set("n", "<A-o>", "<Cmd>NvimTreeToggle<CR>", { desc = "Open file tree" })
