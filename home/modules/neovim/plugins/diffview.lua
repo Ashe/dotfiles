8e81501b6b22369c31085709fb1628f44b62068e
ashe <git@aas.sh>
2026-09-20 15:40:56 +0100

    nvim: Update diffview aesthetics

--- home/modules/neovim/plugins/diffview.lua
+++ home/modules/neovim/plugins/diffview.lua
@@ -19,6 +19,36 @@ require("diffview").setup({
 	},
 })
 
+-- Diffview links its compact file-panel labels to full diff highlights, which
+-- can include distracting backgrounds. Use Neovim's semantic foreground
+-- groups instead so the colours continue to follow the active colorscheme.
+local function set_file_panel_highlights()
+	local links = {
+		DiffviewFilePanelInsertions = "Added",
+		DiffviewFilePanelDeletions = "Removed",
+		DiffviewStatusAdded = "Added",
+		DiffviewStatusUntracked = "Added",
+		DiffviewStatusModified = "Changed",
+		DiffviewStatusRenamed = "Changed",
+		DiffviewStatusCopied = "Changed",
+		DiffviewStatusTypeChanged = "Changed",
+		DiffviewStatusUnmerged = "Changed",
+		DiffviewStatusUnknown = "Removed",
+		DiffviewStatusDeleted = "Removed",
+		DiffviewStatusBroken = "Removed",
+	}
+
+	for group, target in pairs(links) do
+		vim.api.nvim_set_hl(0, group, { link = target })
+	end
+end
+
+set_file_panel_highlights()
+vim.api.nvim_create_autocmd("ColorScheme", {
+	group = vim.api.nvim_create_augroup("UserDiffviewHighlights", { clear = true }),
+	callback = set_file_panel_highlights,
+})
+
 local function in_diffview()
 	return diffview_lib.get_current_view() ~= nil
 end
